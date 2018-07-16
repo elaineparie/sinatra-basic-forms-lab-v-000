16f37ebe95b60533ad5a57651e9c041a5c33e9c1
Elaine Anderson <eparie@bu.edu>
2018-07-16 16:01:47 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,11 +7,11 @@ class App < Sinatra::Base
     erb :index
   end
 
-  get '/create_puppy.erb' do
+  get '/NEW.erb' do
     erb :create_puppy
   end
 
-post '/create_puppy.erb' do
+post '/NEW.erb' do
 @puppy = Puppy.new(params[:name, :breed, :age])
 end
 
